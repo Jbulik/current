2eeeb4983723972df0d2cd00a7dbd6c87b15c78e
Jbulik <bulina_yuliya@bk.ru>
2022-07-10 18:27:44 +0300

    папка 5 семинар

--- EX006_Weght/Program.cs
+++ EX006_Weght/Program.cs
@@ -11,4 +11,4 @@ if ( c >  max) max = c;
 if (d >  max) max = d;
 if ( e >  max) max = e;
 
-Console.WriteLine (max);
+// Console.WriteLine (max);
---
+++ Семинары.Лекция2/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывод случайных чисел c 10 до 99
+
+
+
+int number = new Random().Next(1, 100);
+//Console.Write(number);
+
+int num1 = number / 10;
+int num2 = number % 10;
+
+Console.WriteLine(number + " "+ num1 + ' ' + num2);
+
+
