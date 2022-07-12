d2a1491f1ac6bb9f37b63a87e3dc6bdce0941cff
Jbulik <bulina_yuliya@bk.ru>
2022-07-12 21:49:52 +0300

    задача 34 добавлена, ДЗ к семинару №5 done

--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -40,57 +40,90 @@
   
 //№ 38 задайте одномерный массив случайных чисел. Найти разницу м/д макс и мин элементами
 
+// int[] CreateRandomArray(int size, int min, int max)
+//     {
+//         int[] array = new int[size];
+//         for (int i = 0; i < size; i++)
+//         {
+//             array[i] = new Random().Next(min, max + 1);
+//         }
+//         return array;
+//     }
+
+
+//     void ShowArray(int[] array)
+//     {
+//         Console.Write("[ ");
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             Console.Write(array[i] + " ");
+//         }
+//         Console.Write(" ] -> ");
+        
+//     }
+
+
+//  int DeltaFind (int [] array)
+//     {
+//         int MinNum = array[0];
+//         int Max = array[0];
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//            if (array[i] < MinNum) 
+//            MinNum = array[i];
+                      
+//         }
+
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//            if (array[i] > Max) 
+//             Max = array[i];           
+//         }
+
+//         int delta= Max - MinNum;
+        
+//         return delta;
+        
+//     }
+
+ 
+ 
+//  int[] myArray = CreateRandomArray(5 , 1, 10);
+//   ShowArray(myArray);
+//   Console.WriteLine(DeltaFind(myArray));
+  
+
+  //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
 int[] CreateRandomArray(int size, int min, int max)
     {
         int[] array = new int[size];
         for (int i = 0; i < size; i++)
         {
-            array[i] = new Random().Next(min, max + 1);
+            array[i] = new Random().Next(min, max);
         }
         return array;
     }
 
 
-    void ShowArray(int[] array)
+int QuantityPositive(int[] array){
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ )
     {
-        Console.Write("[ ");
-        for (int i = 0; i < array.Length; i++)
-        {
-            Console.Write(array[i] + " ");
-        }
-        Console.Write(" ] -> ");
-        
-    }
-
-
- int DeltaFind (int [] array)
+    if (array[i] % 2 == 0)
     {
-        int MinNum = array[0];
-        int Max = array[0];
-        for (int i = 0; i < array.Length; i++)
-        {
-           if (array[i] < MinNum) 
-           MinNum = array[i];
-                      
-        }
-
-        for (int i = 0; i < array.Length; i++)
-        {
-           if (array[i] > Max) 
-            Max = array[i];           
-        }
-
-        int delta= Max - MinNum;
-        
-        return delta;
-        
+      quantity++;
     }
+  }
+  return quantity;
+}
 
- 
- 
- int[] myArray = CreateRandomArray(5 , 1, 10);
-  ShowArray(myArray);
-  Console.WriteLine(DeltaFind(myArray));
-  
+ Console.Clear();
+int[] array = CreateRandomArray(4, 100, 900);
+Console.WriteLine(String.Join( " ", array));
+
+int quantity = QuantityPositive (array);
+Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
 
 
